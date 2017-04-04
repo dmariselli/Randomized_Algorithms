0e0b5dbce2bb286b97993175871d137794c0af00
dmariselli <dmariselli@gmail.com>
2017-04-04 11:37:45 -0400

    Added two examples of Randomized Algorithms

---
+++ RandomizedQuickSort.java
@@ -0,0 +1,62 @@
+import java.util.Random;
+
+public class RandomizedQuickSort {
+
+  public static void main(String[] args) {
+
+    int[] array = {24,2,45,20,56,75,2,56,99,53,12};
+    quickSort(array, 0, array.length-1);
+    printArray(array);
+
+  }
+
+  public static void printArray(int[] arr) {
+    for (int digit : arr) {
+      System.out.print(digit + " ");
+    }
+    System.out.println();
+  }
+
+  public static int pickPivot(int low, int high) {
+    Random rand = new Random();
+    return low + rand.nextInt(high-low+1);
+  }
+
+  public static void quickSort(int[] arr, int low, int high) {
+
+    printArray(arr);
+    // int pivot = low + (high - low) / 2;
+    int pivot = pickPivot(low, high);
+    int i = low;
+    int j = high;
+
+    System.out.println("pivot: " + arr[pivot]);
+
+    while (i <= j) {
+      while (arr[i] < arr[pivot]) {
+        i++;
+      }
+
+      while (arr[j] > arr[pivot]) {
+        j--;
+      }
+
+      if (i <= j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        i++;
+        j--;
+      }
+    }
+
+    if (low < j) {
+      quickSort(arr, low, j);
+    }
+    if (i < high) {
+      quickSort(arr, i, high);
+    }
+
+  }
+
+}
